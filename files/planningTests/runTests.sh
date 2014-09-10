c1735a8af35402baa559b7fb244901388a35df07
Luis J. Manso <luis.manso@gmail.com>
2014-09-10 15:01:32 +0200

    Work on RoCKIN domain

--- files/planningTests/runTests.sh
+++ files/planningTests/runTests.sh
@@ -3,7 +3,7 @@ echo "######################################################################"
 planningTest="navigation/hallToPatio"
 echo "Next: $planningTest"
 echo "Press enter to run test"
-read c
+sleep 2
 cd $planningTest
 agglplan ../../../../etc/domain.aggl initialModel.xml target.xml
 cd -
@@ -14,7 +14,7 @@ echo "######################################################################"
 planningTest="perception/findGranny"
 echo "Next: $planningTest"
 echo "Press enter to run test"
-read c
+sleep 2
 cd $planningTest
 agglplan ../../../../etc/domain.aggl initialModel.xml target.xml
 cd -
@@ -25,7 +25,7 @@ echo "######################################################################"
 planningTest="grasp"
 echo "Next: $planningTest 1"
 echo "Press enter to run test"
-read c
+sleep 2
 cd $planningTest
 agglplan ../../../etc/domain.aggl initialModel1.xml target1.xml
 cd -
@@ -36,7 +36,7 @@ echo "######################################################################"
 planningTest="grasp"
 echo "Next: $planningTest 2"
 echo "Press enter to run test"
-read c
+sleep 2
 cd $planningTest
 agglplan ../../../etc/domain.aggl initialModel2.xml target2.xml
 cd -
@@ -47,7 +47,7 @@ echo "######################################################################"
 planningTest="deliver/deliverKnown"
 echo "Next: $planningTest 0"
 echo "Press enter to run test"
-read c
+sleep 2
 cd $planningTest
 agglplan ../../../../etc/domain.aggl initialModel.xml target0.xml
 cd -
@@ -58,7 +58,7 @@ echo "######################################################################"
 planningTest="deliver/deliverKnown"
 echo "Next: $planningTest 1"
 echo "Press enter to run test"
-read c
+sleep 2
 cd $planningTest
 agglplan ../../../../etc/domain.aggl initialModel.xml target1.xml
 cd -
@@ -69,9 +69,20 @@ echo "######################################################################"
 planningTest="deliver/deliverKnown"
 echo "Next: $planningTest 2"
 echo "Press enter to run test"
-read c
+sleep 2
 cd $planningTest
 agglplan ../../../../etc/domain.aggl initialModel.xml target2.xml
+cd -
+
+
+echo
+echo "######################################################################"
+planningTest="hri/coffee"
+echo "Next: $planningTest"
+echo "Press enter to run test"
+sleep 2
+cd $planningTest
+agglplan ../../../../etc/domain.aggl initialModel.xml target.xml
 cd -
 
 
