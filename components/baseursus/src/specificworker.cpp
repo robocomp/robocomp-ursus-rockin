e2afd0224d4ebb7e120d4dfabe0ff0a6ed5f2041
Luis J. Manso <luis.manso@gmail.com>
2015-01-16 14:41:26 +0100

    updateodometry

--- components/baseursus/src/specificworker.cpp
+++ components/baseursus/src/specificworker.cpp
@@ -165,14 +165,16 @@ void SpecificWorker::resetOdometer()
 
 void SpecificWorker::setOdometer(const ::RoboCompOmniRobot::TBaseState &state)
 {
-	innermodel->updateTransformValues("backPose",     state.x, 0, state.z,     0, state.alpha, 0);
-	innermodel->updateTransformValues("newPose",      0,       0,       0,     0,           0, 0);
+	setOdometerPose(state.x, state.z, state.alpha);
 }
 
 void SpecificWorker::setOdometerPose(::Ice::Int x, ::Ice::Int z, ::Ice::Float alpha)
 {
 	innermodel->updateTransformValues("backPose",     x, 0, z,     0, alpha, 0);
 	innermodel->updateTransformValues("newPose",      0, 0, 0,     0,     0, 0);
+	this->x = x;
+	this->z = z;
+	this->angle = alpha;
 }
 
 void SpecificWorker::correctOdometer(::Ice::Int x, ::Ice::Int z, ::Ice::Float alpha)
