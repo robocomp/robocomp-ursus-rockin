83c16297353f09ca5b87e2c13b7376ed19fdce55
robolab <pbustos@unex.es>
2015-02-04 18:35:51 +0100

    fix

--- etc/setDevices.sh
+++ etc/setDevices.sh
@@ -1,8 +1,12 @@
+echo "Faulhaber..."
 faulhaber=`ls -lah /dev/faulhaber | awk '{print $11}'`
 sed -ifaulhaber.back "s/Faulhaber.Device.*/Faulhaber.Device=\/dev\/$faulhaber/g" /home/robocomp/robocomp/components/robocomp-ursus-rockin/etc/faulhaber.conf
 
+
+echo "Dunker..."
 dunker=`ls -lah /dev/dunker | awk '{print $11}'`
 sed -idunker.back "s/dunkermotoren.Device.*/dunkermotoren.Device=\/dev\/$dunker/g" /home/robocomp/robocomp/components/robocomp-ursus-rockin/etc/dunker.conf
 
+echo "Dynamixel..."
 dynamixel=`ls -lah /dev/dynamixel | awk '{print $11}'`
 sed -idynamixel.back "s/Dynamixel.Device.*/Dynamixel.Device=\/dev\/$dynamixel/g" /home/robocomp/robocomp/components/robocomp-ursus-rockin/etc/dynamixel.conf
