c85cc4ce0168350bbd9938e24cbdd65f2ae5d0b0
luiky <kalderabatera@gmail.com>
2015-07-13 13:20:49 +0200

    Class created for agmInner

--- components/agmInnerAgent/src/specificworker.cpp
+++ components/agmInnerAgent/src/specificworker.cpp
@@ -28,33 +28,35 @@ void SpecificWorker::compute()
 //  		innerModel->treePrint();
 // 		qDebug()<<"\n\n*********** include_im *************";
 // 		
-// 		QHash<QString, int32_t>  match1;
-// 		match1.insert("world",20);
+		QHash<QString, int32_t>  match1;
+		match1.insert("world",20);
 // 		//match1.insert("t_table0",5);
 // 		//match1.insert("initialRobotPose",1);
 // 				
-// 		include_im(match1);
+		include_im(match1);
 // 		
 //  		qDebug()<<"\n\nnumberOfSymbols AFTER insert InnerModel"<<worldModel->numberOfSymbols();
 //  		AGMModelPrinter::printWorld(worldModel);
 // 		
-// 		QString nodeName="world";
-//  		qDebug()<<"\n\n****** extract innerModel from:"<<nodeName;
-// 		(extractInnerModel(nodeName))->treePrint();
-// 		qDebug()<<"\n\n**********************";
+		QString nodeName="world";
+ 		qDebug()<<"\n\n****** extract innerModel from:"<<nodeName;
+		//(extractInnerModel(nodeName))->treePrint();
+		agmInner.setWorld(worldModel);
+		(agmInner.extractInnerModel(nodeName))->treePrint();
+		qDebug()<<"\n\n**********************";
 // 		
-// 		printf("sending modification!\n");
-// 		AGMModel::SPtr newModel(new AGMModel(worldModel));		
+		printf("sending modification!\n");
+		AGMModel::SPtr newModel(new AGMModel(worldModel));		
 // 		//AGMModelPrinter::printWorld(newModel);
 // 		sendModificationProposal(worldModel, newModel);
-		int symbolID=20;
-		string linkType ="RT";
-		QList<int> visited;
+// 		int symbolID=20;
+// 		string linkType ="RT";
+// 		QList<int> visited;
 // 		visited.append(symbolID);
 		bool loop=false;
-		checkLoop(symbolID,visited,"RT",loop);
-		qDebug()<<"CheckLoop from"<<symbolID<<"linkType"<<QString::fromStdString(linkType)<<"There is loop ?"<<loop<<visited;
-		qFatal("fary");
+// 		agmInner.checkLoop(symbolID,visited,"RT",loop);
+		//qDebug()<<"CheckLoop from"<<symbolID<<"linkType"<<QString::fromStdString(linkType)<<"There is loop ?"<<loop<<visited;
+// 		qFatal("fary");
 	}
 	
 }	
@@ -92,19 +94,19 @@ int SpecificWorker::findName(QString n)
  * @param imNodeName InnerModelNode name to start the path
  * @return InnerModel* tree from InnerModelNode name
  */
-InnerModel* SpecificWorker::extractInnerModel(QString imNodeName)
-{
-	
-	
-	InnerModel *imNew = new InnerModel() ;
-
-	int symbolID = findName(imNodeName);
-// 	symbolID=5;
-	if (symbolID > -1)
-		recorrer(imNew, symbolID);
-	
-	return imNew;
-}
+// InnerModel* SpecificWorker::extractInnerModel(QString imNodeName)
+// {
+// 	
+// 	
+// 	InnerModel *imNew = new InnerModel() ;
+// 
+// 	int symbolID = findName(imNodeName);
+// // 	symbolID=5;
+// 	if (symbolID > -1)
+// 		recorrer(imNew, symbolID);
+// 	
+// 	return imNew;
+// }
 
 
 
@@ -115,21 +117,21 @@ InnerModel* SpecificWorker::extractInnerModel(QString imNodeName)
  * @param symbolID ID del symbolo punto de partida
  * @return void
  */
-void SpecificWorker::recorrer(InnerModel* imNew, int& symbolID)
-{
-	const AGMModelSymbol::SPtr &symbol = worldModel->getSymbol(symbolID);
-	for (AGMModelSymbol::iterator edge_itr=symbol->edgesBegin(worldModel); edge_itr!=symbol->edgesEnd(worldModel); edge_itr++)
-	{
-		//std::cout<<(*edge_itr).toString(worldModel)<<"\n";
-		//comprobamos el id del simbolo para evitar los arcos que le llegan y seguir solo los que salen del nodo
-		if ((*edge_itr)->getLabel() == "RT" && (*edge_itr)->getSymbolPair().first==symbolID )
-		{
-			int second = (*edge_itr)->getSymbolPair().second;
-			edgeToInnerModel((*edge_itr),imNew);
-			recorrer(imNew,second);
-		}
-	}
-}
+// void SpecificWorker::recorrer(InnerModel* imNew, int& symbolID)
+// {
+// 	const AGMModelSymbol::SPtr &symbol = worldModel->getSymbol(symbolID);
+// 	for (AGMModelSymbol::iterator edge_itr=symbol->edgesBegin(worldModel); edge_itr!=symbol->edgesEnd(worldModel); edge_itr++)
+// 	{
+// 		//std::cout<<(*edge_itr).toString(worldModel)<<"\n";
+// 		//comprobamos el id del simbolo para evitar los arcos que le llegan y seguir solo los que salen del nodo
+// 		if ((*edge_itr)->getLabel() == "RT" && (*edge_itr)->getSymbolPair().first==symbolID )
+// 		{
+// 			int second = (*edge_itr)->getSymbolPair().second;
+// 			edgeToInnerModel((*edge_itr),imNew);
+// 			recorrer(imNew,second);
+// 		}
+// 	}
+// }
 
 /**
  * @brief Recorre el grafo por los enlaces RT desde el symbolID creando un innerModel equivalente
@@ -164,32 +166,32 @@ void SpecificWorker::recorrer(InnerModel* imNew, int& symbolID)
 // 	
 // }
 
-void SpecificWorker::checkLoop(int& symbolID, QList<int> &visited, string linkType, bool &loop)
-{
-	if (visited.contains(symbolID) )
-	{
-		loop=true;
-		visited.append(symbolID);
-		return;
-	}
-	else
-		visited.append(symbolID);
-	
-	const AGMModelSymbol::SPtr &symbol = worldModel->getSymbol(symbolID);
-	for (AGMModelSymbol::iterator edge_itr=symbol->edgesBegin(worldModel); edge_itr!=symbol->edgesEnd(worldModel); edge_itr++)
-	{
-		//std::cout<<(*edge_itr).toString(worldModel)<<"\n";
-		//comprobamos el id del simbolo para evitar los arcos que le llegan y seguir solo los que salen del nodo
-		if ((*edge_itr)->getLabel() == linkType && (*edge_itr)->getSymbolPair().first==symbolID )
-		{
-			int second = (*edge_itr)->getSymbolPair().second;
-			qDebug()<<symbolID<<"--"<<QString::fromStdString(linkType)<<"-->"<<second;
-			qDebug()<<"\tvisited"<<visited;									
-			checkLoop(second,visited, linkType,loop);
-		}
-	}
-	
-}
+// void SpecificWorker::checkLoop(int& symbolID, QList<int> &visited, string linkType, bool &loop)
+// {
+// 	if (visited.contains(symbolID) )
+// 	{
+// 		loop=true;
+// 		visited.append(symbolID);
+// 		return;
+// 	}
+// 	else
+// 		visited.append(symbolID);
+// 	
+// 	const AGMModelSymbol::SPtr &symbol = worldModel->getSymbol(symbolID);
+// 	for (AGMModelSymbol::iterator edge_itr=symbol->edgesBegin(worldModel); edge_itr!=symbol->edgesEnd(worldModel); edge_itr++)
+// 	{
+// 		//std::cout<<(*edge_itr).toString(worldModel)<<"\n";
+// 		//comprobamos el id del simbolo para evitar los arcos que le llegan y seguir solo los que salen del nodo
+// 		if ((*edge_itr)->getLabel() == linkType && (*edge_itr)->getSymbolPair().first==symbolID )
+// 		{
+// 			int second = (*edge_itr)->getSymbolPair().second;
+// 			qDebug()<<symbolID<<"--"<<QString::fromStdString(linkType)<<"-->"<<second;
+// 			qDebug()<<"\tvisited"<<visited;									
+// 			checkLoop(second,visited, linkType,loop);
+// 		}
+// 	}
+// 	
+// }
 
 /**
  * @brief ..transform the information contains in an AGM edge in two InnerModelNode, adding the information stored in the label RT 
@@ -199,51 +201,51 @@ void SpecificWorker::checkLoop(int& symbolID, QList<int> &visited, string linkTy
  * @param imNew ...
  * @return void
  */
-void SpecificWorker::edgeToInnerModel(AGMModelEdge edge, InnerModel* imNew) 
-{
-	InnerModelNode* nodeA = NULL;
-	InnerModelNode* nodeB = NULL;
-
-	int first = edge->getSymbolPair().first;
-	int second = edge->getSymbolPair().second;	
-
-	const AGMModelSymbol::SPtr &symbolA = worldModel->getSymbol(first);
-	const AGMModelSymbol::SPtr &symbolB = worldModel->getSymbol(second);
-
-	QString nameA = QString::fromStdString(symbolA->attributes["name"]);
-	QString nameB = QString::fromStdString(symbolB->attributes["name"]);
-	
-// 	qDebug()<<"insertar en new InnerModel "<<nameA<<"--"<< QString::fromStdString ( edge->getLabel() ) <<"-->"<<nameB;//<<tx<<ty<<tz<<rx<<ry<<rz;
-// 	qDebug()<<"equivalente al enlace en AGM "<<QString::fromStdString (symbolA->toString())<<"--"<< QString::fromStdString ( edge->getLabel() ) <<"-->"<<QString::fromStdString (symbolB->toString());//<<tx<<ty<<tz<<rx<<ry<<rz;
-	
-	
-	//node father
-	nodeA=imNew->getNode(nameA);
-	//entiendo que sino exite lo cuelgo del root, Estará vacio...
-	if (nodeA==NULL)
-	{
-		nodeA = imNew->newTransform(nameA, "static",imNew->getRoot());
-		imNew->getRoot()->addChild(nodeA);
-// 		qDebug()<<"NODE A ERA NULL NO EXISTIA"<<nameA<<"node A->print(verbose):";
-// 		nodeA->print(true);
-// 		qDebug()<<"---------";
-	}
-	//TODO hacer que por defecto si en el .xml de agm no se especifican valores de RT ponerlos a 0.
-	float tx,ty,tz,rx,ry,rz;
-	tx=ty=tz=rx=ry=rz=0.;
-	tx = str2float(edge->attributes["tx"]);
-	ty = str2float(edge->attributes["ty"]);
-	tz = str2float(edge->attributes["tz"]);
-
-	rx = str2float(edge->attributes["rx"]);
-	ry = str2float(edge->attributes["ry"]);
-	rz = str2float(edge->attributes["rz"]);
-	nodeB = imNew->newTransform (nameB, "static",nodeA,tx,ty,tz,rx,ry,rz);	
-	if (nodeB==NULL)
-		qFatal("MAAAAAL edgeToInnerModel() nodeB == null ");
-	nodeA->addChild(nodeB);
-
-}
+// void SpecificWorker::edgeToInnerModel(AGMModelEdge edge, InnerModel* imNew) 
+// {
+// 	InnerModelNode* nodeA = NULL;
+// 	InnerModelNode* nodeB = NULL;
+// 
+// 	int first = edge->getSymbolPair().first;
+// 	int second = edge->getSymbolPair().second;	
+// 
+// 	const AGMModelSymbol::SPtr &symbolA = worldModel->getSymbol(first);
+// 	const AGMModelSymbol::SPtr &symbolB = worldModel->getSymbol(second);
+// 
+// 	QString nameA = QString::fromStdString(symbolA->attributes["name"]);
+// 	QString nameB = QString::fromStdString(symbolB->attributes["name"]);
+// 	
+// // 	qDebug()<<"insertar en new InnerModel "<<nameA<<"--"<< QString::fromStdString ( edge->getLabel() ) <<"-->"<<nameB;//<<tx<<ty<<tz<<rx<<ry<<rz;
+// // 	qDebug()<<"equivalente al enlace en AGM "<<QString::fromStdString (symbolA->toString())<<"--"<< QString::fromStdString ( edge->getLabel() ) <<"-->"<<QString::fromStdString (symbolB->toString());//<<tx<<ty<<tz<<rx<<ry<<rz;
+// 	
+// 	
+// 	//node father
+// 	nodeA=imNew->getNode(nameA);
+// 	//entiendo que sino exite lo cuelgo del root, Estará vacio...
+// 	if (nodeA==NULL)
+// 	{
+// 		nodeA = imNew->newTransform(nameA, "static",imNew->getRoot());
+// 		imNew->getRoot()->addChild(nodeA);
+// // 		qDebug()<<"NODE A ERA NULL NO EXISTIA"<<nameA<<"node A->print(verbose):";
+// // 		nodeA->print(true);
+// // 		qDebug()<<"---------";
+// 	}
+// 	//TODO hacer que por defecto si en el .xml de agm no se especifican valores de RT ponerlos a 0.
+// 	float tx,ty,tz,rx,ry,rz;
+// 	tx=ty=tz=rx=ry=rz=0.;
+// 	tx = str2float(edge->attributes["tx"]);
+// 	ty = str2float(edge->attributes["ty"]);
+// 	tz = str2float(edge->attributes["tz"]);
+// 
+// 	rx = str2float(edge->attributes["rx"]);
+// 	ry = str2float(edge->attributes["ry"]);
+// 	rz = str2float(edge->attributes["rz"]);
+// 	nodeB = imNew->newTransform (nameB, "static",nodeA,tx,ty,tz,rx,ry,rz);	
+// 	if (nodeB==NULL)
+// 		qFatal("MAAAAAL edgeToInnerModel() nodeB == null ");
+// 	nodeA->addChild(nodeB);
+// 
+// }
 
 
 /**
@@ -389,6 +391,7 @@ SpecificWorker::SpecificWorker(MapPrx& mprx) : GenericWorker(mprx)
 	active = false;
 	worldModel = AGMModel::SPtr(new AGMModel());
 	worldModel->name = "worldModel";
+	//agmInner = new AgmInner();
 	setPeriod(1000);
 }
 
